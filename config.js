8065faf2cf3374eb7abcb3b19d9f197fef333c59
Dark_Sahan-x_team <120746548+sahan1111@users.noreply.github.com>
2024-10-17 07:49:32 +0000

    Update config.js

--- config.js
+++ config.js
@@ -5,6 +5,6 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "",
+SESSION_ID: process.env.SESSION_ID || "𝙰𝚂𝙸𝚃𝙷𝙰-𝙼𝙳=yrRXlBDZ#ZrwliUSh_ufoVOlBwO-yKt9oF_7Mc9XzVErMp0J24nU",
 MONGODB: process.env.MONGODB || "mongodb+srv://kulathungaasitha319:yjHB0DvFfStNfwPS@cluster0.3oijd.mongodb.net/",//enter mongo db url
 };
